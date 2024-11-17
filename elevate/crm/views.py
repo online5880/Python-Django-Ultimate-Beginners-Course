d16a0938cae204ab82c88c40fcc698b2f533ae57
online5880 <gjtjqkr1@naver.com>
2024-11-17 12:04:19 +0900

    DTL - {% %}

--- elevate/crm/views.py
+++ elevate/crm/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    context = {
+        "first_name":"Kate Johnson"
+    }
+    return render(request,"crm/index.html",context)
+
+def register(request):
+    return render(request,'crm/register.html')
--- elevate/elevate/urls.py
+++ elevate/elevate/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from django.http import HttpResponse
 
-def register(request):
-    return HttpResponse("이건 등록 페이지")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register',register)
+    path("",include('crm.urls'))
 ]
