5a9967818e5a944d5f59e218fc301b709d1a69b5
online5880 <gjtjqkr1@naver.com>
2024-11-17 13:31:15 +0900

    Forms

--- elevate/crm/urls.py
+++ elevate/crm/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("",views.homepage),
-    path("register",views.register),
-    path('task',views.task)
+    path("",views.homepage,name=""),
+    path("register",views.register,name="register"),
+    path('task',views.task,name='task'),
+    path('task-form',views.task_form,name='task-form')
 ]
--- elevate/crm/views.py
+++ elevate/crm/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Task
-
+from .forms import TaskForm
 # Create your views here.
 def homepage(request):
     return render(request,"crm/index.html")
@@ -17,3 +17,8 @@ def task(request):
 def register(request):
     return render(request,'crm/register.html')
 
+def task_form(request):
+    form = TaskForm()
+    
+    context = {"TaskForm":form}
+    return render(request,'crm/task-form.html',context)
